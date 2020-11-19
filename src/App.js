33c7661f9578d8e5e7476c5b362ea92a63c8018d
abdelmawlaSouat <abdelmawla.souat@gmail.com>
2020-11-19 11:54:08 +0100

    Setup Project (dependencies,fonts, color, images)

---
+++ src/App.js
@@ -0,0 +1,23 @@
+/*
+ * App.js
+ *
+ * Author: Abdelmawla Souat (abdelmawla.souat@gmail.com)
+ * Created on 2020/11/19 10:55:45
+ *
+ * Copyright (c) 2020 Shuriken
+ */
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Langing Page</h1>
+        <p>Paraph de test</p>
+       </header>
+    </div>
+  )
+}
+
+export default App;
